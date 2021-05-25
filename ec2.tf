4263ce9069a8b7150a23549591d83c893324b719
sfidahussain <sanfidahussain@gmail.com>
2021-05-25 12:04:39 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_version = ">= 0.14.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "3.26.0"
+        }
+    }
+}
+
 resource "aws_instance" "app-dev" {
   ami = "ami-0d5eff06f840b45e9"
   instance_type = "t2.micro"
