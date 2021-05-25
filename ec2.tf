11e3aeb923992070e1d48d39a63c49c63127f0d3
sfidahussain <sanfidahussain@gmail.com>
2021-05-25 12:08:29 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,16 @@
 terraform {
-    required_version = ">= 0.14.0"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "3.26.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
+
+# Configure the AWS Provider
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
 resource "aws_instance" "app-dev" {
   ami = "ami-0d5eff06f840b45e9"
